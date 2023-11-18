ade8851065ec912067bb04300cc6c8f0e32c688b
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-11-18 12:19:31 +0800

    feat: Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# inlay-hints.nvim
+
+A plugin to simplify enabling neovim offical [inlay hints](https://github.com/neovim/neovim/pull/23426)
---
+++ lua/inlay-hints/init.lua
@@ -0,0 +1,4 @@
+if not vim.fn.has("nvim-0.10") then
+  vim.notify("This plugin only works with neovim 0.10+(nightly), please update your neovim version")
+  return
+end
