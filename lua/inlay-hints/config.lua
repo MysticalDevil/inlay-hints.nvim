6c859b7fb426df516202d24d8a9f0b213eb3950f
MysticalDevil <jinpeng7981@outlook.com>
2026-04-23 03:43:57 +0800

    refactor: remove redundant empty table check

--- lua/inlay-hints/config.lua
+++ lua/inlay-hints/config.lua
@@ -10,14 +10,12 @@ local config = {
 config.load = function(user_config)
   config.options = vim.tbl_deep_extend("force", default_config, user_config or {})
 
-  if next(config.options) then
-    if config.options.commands.enable then
-      require("inlay-hints.commands")
-    end
+  if config.options.commands.enable then
+    require("inlay-hints.commands")
+  end
 
-    if config.options.autocmd.enable then
-      require("inlay-hints.utils").enable_inlay_hints_autocmd()
-    end
+  if config.options.autocmd.enable then
+    require("inlay-hints.utils").enable_inlay_hints_autocmd()
   end
 end
 
