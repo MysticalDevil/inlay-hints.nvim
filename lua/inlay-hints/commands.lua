4af2608404fcff309c727eb4597cd5997e6a29d5
MysticalDevil <jinpeng7981@outlook.com>
2026-04-23 21:35:49 +0800

    fix: correct user command descriptions

--- lua/inlay-hints/commands.lua
+++ lua/inlay-hints/commands.lua
@@ -3,15 +3,15 @@ local create_user_command = vim.api.nvim_create_user_command
 create_user_command(
   "InlayHintsToggle",
   require("inlay-hints.utils").toggle_inlay_hints,
-  { desc = "Enable/Disable inlay hints on current buffer" }
+  { desc = "Toggle inlay hints on current buffer" }
 )
 create_user_command(
   "InlayHintsEnable",
   require("inlay-hints.utils").enable_inlay_hints,
-  { desc = "Enable/Disable inlay hints on current buffer" }
+  { desc = "Enable inlay hints on current buffer" }
 )
 create_user_command(
   "InlayHintsDisable",
   require("inlay-hints.utils").disable_inlay_hints,
-  { desc = "Enable/Disable inlay hints on current buffer" }
+  { desc = "Disable inlay hints on current buffer" }
 )
